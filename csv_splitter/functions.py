92553edf2cd15e1aeb8b235ebf95f566d7064192
Manuel Galli <gmanuel89@gmail.com>
2022-03-30 14:51:58 +0200

    the UI is better

--- csv_splitter/functions.py
+++ csv_splitter/functions.py
@@ -1,8 +1,9 @@
 ## Import libraries
 from csv_splitter.libraries import *
 
-# Initialise output folder
+# Initialise variables
 output_folder = os.getcwd()
+input_file = ""
 
 
 ## Define the input CSV file
@@ -16,6 +17,7 @@ def select_input_csv_file():
     # Just to confirm...
     Tk().withdraw()
     messagebox.showinfo(title='CSV file selected', message="The CSV file selected is '%s'" %(input_file))
+    return input_file
 
 
 ## Define where to save the CSV files
@@ -35,6 +37,7 @@ def select_output_folder():
     # Just to confirm...
     Tk().withdraw()
     messagebox.showinfo(title='Folder selected', message="The CSV file(s) will be saved in '%s'" %(output_folder))
+    return output_folder
 
 
 ## Show repository info
@@ -106,117 +109,3 @@ def split_csv_file_into_chunks(number_of_output_chunks):
         # Success
         Tk().withdraw()
         messagebox.showinfo(title='Success!', message='The input CSV has been successfully split into %s chunks!' %(number_of_output_chunks))
-
-
-# Get fonts for UI
-def get_ui_fonts(window):
-    ### Store the fonts in a variable for faster editing
-    # Get system info (Platform - Release - Version (- Linux Distro))
-    system_os = platform.system()
-    os_release = platform.release()
-    os_version = platform.version()
-    # Default sizes (determined on a 1680x1050 screen) (in order to make them adjust to the size screen, the screen resolution should be retrieved)
-    #title_font_size = 24
-    #other_font_size = 11
-    screen_width = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
-    # Determine the font size according to the resolution
-    total_number_of_pixels = screen_width * screen_height
-    # Determine the scaling factor (according to a complex formula)
-    scaling_factor_title_font = float((0.03611 * total_number_of_pixels) + 9803.1254)
-    scaling_factor_other_font = float((0.07757 * total_number_of_pixels) + 23529.8386)
-    title_font_size = int(round(total_number_of_pixels / scaling_factor_title_font))
-    other_font_size = int(round(total_number_of_pixels / scaling_factor_other_font))
-    # Windows
-    if system_os == "Windows":
-        # Define the fonts
-        garamond_title_bold = font.Font(family = "Garamond", size = title_font_size, weight = "bold")
-        garamond_other_normal = font.Font(family = "Garamond", size = other_font_size, weight = "normal")
-        garamond_other_bold = font.Font(family = "Garamond", size = other_font_size, weight = "bold")
-        arial_title_bold = font.Font(family = "Arial", size = title_font_size, weight = "bold")
-        arial_other_normal = font.Font(family = "Arial", size = other_font_size, weight = "normal")
-        arial_other_bold = font.Font(family = "Arial", size = other_font_size, weight = "bold")
-        trebuchet_title_bold = font.Font(family = "Trebuchet MS", size = title_font_size, weight = "bold")
-        trebuchet_other_normal = font.Font(family = "Trebuchet MS", size = other_font_size, weight = "normal")
-        trebuchet_other_bold = font.Font(family = "Trebuchet MS", size = other_font_size, weight = "bold")
-        calibri_title_bold = font.Font(family = "Calibri", size = title_font_size, weight = "bold")
-        calibri_other_normal = font.Font(family = "Calibri", size = other_font_size, weight = "normal")
-        calibri_other_bold = font.Font(family = "Calibri", size = other_font_size, weight = "bold")
-        # Use them in the GUI
-        title_font = calibri_title_bold
-        label_font = calibri_other_normal
-        entry_font = calibri_other_normal
-        button_font = calibri_other_bold
-    # Linux
-    elif system_os == "Linux":
-        # Retrieve the linux distribution
-        linux_distro = platform.linux_distribution()
-        # Ubuntu
-        if "Ubuntu" in linux_distro or "Ubuntu" in os_version:
-            # Define the fonts
-            ubuntu_title_bold = font.Font(family = "Ubuntu", size = title_font_size, weight = "bold")
-            ubuntu_other_normal = font.Font(family = "Ubuntu", size = other_font_size, weight = "normal")
-            ubuntu_other_bold = font.Font(family = "Ubuntu", size = other_font_size, weight = "bold")
-            cantarell_title_bold = font.Font(family = "Cantarell", size = title_font_size, weight = "bold")
-            cantarell_other_normal = font.Font(family = "Cantarell", size = other_font_size, weight = "normal")
-            cantarell_other_bold = font.Font(family = "Cantarell", size = other_font_size, weight = "bold")
-            bitstream_charter_title_bold = font.Font(family = "Bitstream Charter", size = title_font_size, weight = "bold")
-            bitstream_charter_other_normal = font.Font(family = "Bitstream Charter", size = other_font_size, weight = "normal")
-            bitstream_charter_other_bold = font.Font(family = "Bitstream Charter", size = other_font_size, weight = "bold")
-            liberation_title_bold = font.Font(family = "Liberation Sans", size = title_font_size, weight = "bold")
-            liberation_other_normal = font.Font(family = "Liberation Sans", size = other_font_size, weight = "normal")
-            liberation_other_bold = font.Font(family = "Liberation Sans", size = other_font_size, weight = "bold")
-            # Use them in the GUI
-            title_font = ubuntu_title_bold
-            label_font = ubuntu_other_normal
-            entry_font = ubuntu_other_normal
-            button_font = ubuntu_other_bold
-        # Fedora
-        elif "Fedora" in linux_distro or "Fedora" in os_version:
-            # Define the fonts
-            liberation_title_bold = font.Font(family = "Liberation Sans", size = title_font_size, weight = "bold")
-            liberation_other_normal = font.Font(family = "Liberation Sans", size = other_font_size, weight = "normal")
-            liberation_other_bold = font.Font(family = "Liberation Sans", size = other_font_size, weight = "bold")
-            cantarell_title_bold = font.Font(family = "Cantarell", size = title_font_size, weight = "bold")
-            cantarell_other_normal = font.Font(family = "Cantarell", size = other_font_size, weight = "normal")
-            cantarell_other_bold = font.Font(family = "Cantarell", size = other_font_size, weight = "bold")
-            # Use them in the GUI
-            title_font = cantarell_title_bold
-            label_font = cantarell_other_normal
-            entry_font = cantarell_other_normal
-            button_font = cantarell_other_bold
-        # Other linux distros
-        else:
-            # Define the fonts
-            liberation_title_bold = font.Font(family = "Liberation Sans", size = title_font_size, weight = "bold")
-            liberation_other_normal = font.Font(family = "Liberation Sans", size = other_font_size, weight = "normal")
-            liberation_other_bold = font.Font(family = "Liberation Sans", size = other_font_size, weight = "bold")
-            # Use them in the GUI
-            title_font = liberation_title_bold
-            label_font = liberation_other_normal
-            entry_font = liberation_other_normal
-            button_font = liberation_other_bold
-    elif system_os == "Mac":
-        # Define the fonts
-        helvetica_title_bold = font.Font(family = "Helvetica", size = title_font_size, weight = "bold")
-        helvetica_other_normal = font.Font(family = "Helvetica", size = other_font_size, weight = "normal")
-        helvetica_other_bold = font.Font(family = "Helvetica", size = other_font_size, weight = "bold")
-        # Use them in the GUI
-        title_font = helvetica_title_bold
-        label_font = helvetica_other_normal
-        entry_font = helvetica_other_normal
-        button_font = helvetica_other_bold
-    else:
-        # Define the fonts
-        courier_title_bold = font.Font(family = "Courier New", size = title_font_size, weight = "bold")
-        courier_other_normal = font.Font(family = "Courier New", size = other_font_size, weight = "normal")
-        courier_other_bold = font.Font(family = "Courier New", size = other_font_size, weight = "bold")
-        # Use them in the GUI
-        title_font = courier_title_bold
-        label_font = courier_other_normal
-        entry_font = courier_other_normal
-        button_font = courier_other_bold
-    #font.families(root = window)
-    # return
-    ui_fonts = {'title_font': title_font, 'label_font':label_font, 'entry_font': entry_font, 'button_font': button_font}
-    return ui_fonts
