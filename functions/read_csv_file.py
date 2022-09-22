ebbd8a6a08866e2ba3d3db31407577eda262783e
Manuel Galli <gmanuel89@gmail.com>
2022-09-22 16:43:36 +0200

    implemented functions to read/write CSV (modular)

---
+++ functions/read_csv_file.py
@@ -0,0 +1,49 @@
+## Import libraries
+import csv
+import sys
+
+## Read input csv file (returns a list of rows)
+def read_csv_file(input_csv_file: str, output_format='list') -> list[list] | list[dict]:
+    ## Open the CSV file
+    with open(input_csv_file, 'r', encoding='UTF8') as input_file:
+        # Prevent possible errors due to large columns (beyond 131072 characters)
+        try:
+            if output_format == 'list':
+                input_csv_file_lines = list(csv.reader(input_file))
+            elif output_format == 'dictionary':
+                input_csv_file_dictionary = csv.DictReader(input_file)
+                input_csv_file_lines = []
+                for row in input_csv_file_dictionary:
+                    input_csv_file_lines.append(dict(row))
+            else:
+                input_csv_file_lines = csv.reader(input_file)
+        except:
+            print("Presence of too large cells!!!")
+            field_size_limit = sys.maxsize
+            while True:
+                try:
+                    csv.field_size_limit(field_size_limit)
+                    break
+                except:
+                    field_size_limit = int(field_size_limit / 10)
+            if output_format == 'list':
+                input_csv_file_lines = list(csv.reader(input_file))
+            elif output_format == 'dictionary':
+                input_csv_file_dictionary = csv.DictReader(input_file)
+                input_csv_file_lines = []
+                for row in input_csv_file_dictionary:
+                    input_csv_file_lines.append(dict(row))
+            else:
+                input_csv_file_lines = csv.reader(input_file)
+    ## Bring the row lengths on par
+    if output_format == 'list':
+        csv_column_header = input_csv_file_lines[0]
+        csv_column_number = len(csv_column_header)
+        for r in range(len(input_csv_file_lines)):
+            if len(input_csv_file_lines[r]) < csv_column_number:
+                for cdiff in range(csv_column_number - len(input_csv_file_lines[r])):
+                    input_csv_file_lines[r].append(None)
+    else:
+        pass
+    # return
+    return input_csv_file_lines
--- functions/split_csv_file_into_chunks.py
+++ functions/split_csv_file_into_chunks.py
@@ -1,6 +1,8 @@
 ## Import libraries
 import csv, os, math
 from tkinter import Tk, messagebox
+from functions.read_csv_file import read_csv_file
+from functions.write_csv_file import write_csv_file
 
 ## Function to open the original CSV file and split it
 def split_csv_file_into_chunks(input_file: str, input_file_name: str, number_of_output_chunks: int, output_folder: str):
@@ -9,42 +11,45 @@ def split_csv_file_into_chunks(input_file: str, input_file_name: str, number_of_
     print('INPUT FILE NAME: ' + input_file_name)
     print('OUTPUT FOLDER: ' + output_folder)
     # Open the CSV file and split it into chunks
-    with open (input_file, 'r', encoding='UTF8') as input_csv_file:
-        input_csv_file_lines = list(csv.reader(input_csv_file))
-        # Store the header (to be placed at every csv)
-        csv_header = input_csv_file_lines[0]
-        # The actual lines are beyond the header
-        number_of_lines = len(input_csv_file_lines) - 1
-        print ('number of lines: ', number_of_lines)
-        number_of_lines_per_chunk = math.ceil(number_of_lines / number_of_output_chunks)
-        print ('number of lines per chunk: ', number_of_lines_per_chunk)
-        # Initialise index variables
-        lower_index = None
-        higher_index = None
-        # Start cycling after the header
-        for i in range(number_of_output_chunks):
-            if lower_index is None : lower_index = 1
-            else : lower_index = lower_index + number_of_lines_per_chunk
-            if higher_index is None : higher_index = number_of_lines_per_chunk + 1
-            else: higher_index = higher_index + number_of_lines_per_chunk
-            # Do not go over the length of the file lines
-            if higher_index >= number_of_lines + 1 : higher_index = number_of_lines + 1
-            # Retrieve the lines to be put into the output
-            output_lines = input_csv_file_lines[lower_index:higher_index]
-            print('Number of lines in iteration n.' + str(i+1) + ': ', str(len(output_lines)))
-            # Break out if there are no more lines
-            if len(output_lines) == 0: break
-            # Output file name
-            output_file_name = '(' + str(i+1) + ')' + ' ' + input_file_name
-            # Go to the output folder and save the files
-            os.chdir(output_folder)
-            with open (output_file_name, 'w', encoding='UTF8', newline='') as output_file:
-                csv_writer = csv.writer(output_file)
-                csv_writer.writerow(csv_header)
-                for line in output_lines:
-                    csv_writer.writerow(line)
-                output_file.close()
-        input_csv_file.close()
-        # Success
-        Tk().withdraw()
-        messagebox.showinfo(title='Success!', message='The input CSV has been successfully split into %s chunks!' %(number_of_output_chunks))
+    input_csv_file_lines = read_csv_file(input_file, output_format='list')
+    # Store the header (to be placed at every csv)
+    csv_header = input_csv_file_lines[0]
+    # The actual lines are beyond the header
+    number_of_lines = len(input_csv_file_lines) - 1
+    print ('number of lines: ', number_of_lines)
+    number_of_lines_per_chunk = math.ceil(number_of_lines / number_of_output_chunks)
+    print ('number of lines per chunk: ', number_of_lines_per_chunk)
+    # Initialise index variables
+    lower_index = None
+    higher_index = None
+    # Start cycling after the header
+    for i in range(number_of_output_chunks):
+        if lower_index is None : lower_index = 1
+        else : lower_index = lower_index + number_of_lines_per_chunk
+        if higher_index is None : higher_index = number_of_lines_per_chunk + 1
+        else: higher_index = higher_index + number_of_lines_per_chunk
+        # Do not go over the length of the file lines
+        if higher_index >= number_of_lines + 1 : higher_index = number_of_lines + 1
+        # Retrieve the lines to be put into the output
+        output_lines = input_csv_file_lines[lower_index:higher_index]
+        print('Number of lines in iteration n.' + str(i+1) + ': ', str(len(output_lines)))
+        # Break out if there are no more lines
+        if len(output_lines) == 0: break
+        # Output file name
+        output_file_name = '(' + str(i+1) + ')' + ' ' + input_file_name
+        # Go to the output folder and save the files
+        os.chdir(output_folder)
+        output_lines_with_header = output_lines
+        output_lines_with_header.insert(0, csv_header)
+        write_csv_file(output_lines_with_header, output_file_name)
+        """
+        with open (output_file_name, 'w', encoding='UTF8', newline='') as output_file:
+            csv_writer = csv.writer(output_file)
+            csv_writer.writerow(csv_header)
+            for line in output_lines:
+                csv_writer.writerow(line)
+            output_file.close()
+        """
+    # Success
+    Tk().withdraw()
+    messagebox.showinfo(title='Success!', message='The input CSV has been successfully split into %s chunks!' %(number_of_output_chunks))
